ba3ab8545db7fa0d5ad866a28f2677bade5f93ca
jeongjin984 <philipwq3963@gmail.com>
2023-12-25 17:56:17 +0900

    m

---
+++ code/front-ui/src/app/page.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <input placeholder="로그인"/>
+        <button>로그인</button>
+      </div>
+      <div>
+        <input placeholder='상품ID'/>
+        <button>구매</button>
+      </div>
+      <div>
+        <input placeholder='상품등록'/>
+        <button>등록</button>
+      </div>
+    </div>
+  )
+}
